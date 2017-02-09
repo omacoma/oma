67abb20432c7a155f27791a958b95823ec4623de
Temirlan Omarov <o.temirlan98@gmail.com>
2017-02-08 23:36:02 -0700

    changes week 1

--- Week 1/Program.cs/Program.cs/Program.cs
+++ Week 1/Program.cs/Program.cs/Program.cs
@@ -15,13 +15,8 @@ namespace Program.cs
             string[] arr = s.Split();//split each number as string
             foreach (string i in arr)//each number but it is string yet
             {
-                int z = 1, x = 0;
-                for (int j = 1; j < i.Length; j++) z *= 10;//how many digits does this number have
-                foreach (char c in i)//each digit
-                {
-                    x += (c - '0') * z;//char to a number
-                    z /= 10;
-                }
+                int x = 0;
+                x = int.Parse(i);
                 for (int j = 2; j <= (int)Math.Sqrt(x); j++)//every number%1=0 that's why we begin with 2 
                 {
                     if (x % j == 0) break;//is it prime
